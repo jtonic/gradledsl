9eea75512040ef1f29fe9e9cd2bbacccce4e25b8
jtonic <antonel.pazargic@gmail.com>
2017-10-12 22:41:53 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    application
+    java
+}
+
+application {
+    mainClassName = "ro.jtonic.tests.gradlekt.MyClass"
+}
---
+++ src/main/java/ro/jtonic/tests/gradlekt/MyClass.java
@@ -0,0 +1,13 @@
+package ro.jtonic.tests.gradlekt;
+
+/**
+ * Created by Antonel Ernest Pazargic on 12/10/2017.
+ *
+ * @author Antonel Ernest Pazargic
+ */
+public class MyClass {
+
+  public static void main(String[] args) {
+    System.out.println("Test from java main class");
+  }
+}
